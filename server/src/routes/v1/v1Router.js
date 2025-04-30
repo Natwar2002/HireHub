59b1192cf3cd1d0c64fcc3e3920b4e66dae4f09f
aditya bhojane <bhojaneaditya7@gmail.com>
2025-04-30 22:38:51 +0530

    add update sync

--- server/src/routes/v1/v1Router.js
+++ server/src/routes/v1/v1Router.js
@@ -4,7 +4,7 @@ import adminRouter from "./adminRouter";
 
 const v1Router = Router();
 
-v1Router.use('/v1',adminRouter);
-v1Router.use('/v1', authRouter);
+v1Router.use('/admin',adminRouter);
+v1Router.use('/auth', authRouter);
 
 export default v1Router;
