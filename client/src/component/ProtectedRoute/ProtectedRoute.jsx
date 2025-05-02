899355313c940681d5dd16efe3ed6fbe9f0fe240
Natwar Patidar <natvarp6062@gmail.com>
2025-05-02 21:33:41 +0530

    Add redux setup and frontend auth

---
+++ client/src/component/ProtectedRoute/ProtectedRoute.jsx
@@ -0,0 +1,17 @@
+import { LucideLoader2 } from 'lucide-react';
+import { Navigate } from 'react-router-dom';
+import store from '../../redux/store';
+
+export const ProtectedRoute = ({ children }) => {
+    const { user, token, isLoading } = store.getState().auth;
+
+    if(!user || !token) {
+        return <Navigate to='/auth/signin' />;
+    }
+
+    if(isLoading) {
+        return <div><LucideLoader2 className='animate-spin ml-2' /></div>;
+    }
+
+    return children;
+};
--- client/src/component/siteLoader/SiteLoader.jsx
+++ client/src/component/siteLoader/SiteLoader.jsx
@@ -1,26 +1,32 @@
-import React, { useEffect } from "react";
+import { useEffect, useState } from "react";
 import {  Progress } from "@heroui/react";
 import { useNavigate } from "react-router-dom";
+import store from '../../redux/store';
 
 export default function SiteLoader() {
-  const [value, setValue] = React.useState(0);
+  const [value, setValue] = useState(0);
   const navigate = useNavigate();
+  const { user, token } = store.getState().auth;
 
-  React.useEffect(() => {
+  useEffect(() => {
     const interval = setInterval(() => {
-      if(value == 100) {
-        return () => clearInterval(interval)
+      if(value >= 100) {
+        clearInterval(interval)
+        return;
       }
       setValue((v) => (v >= 100 ? 0 : v + 10));
     }, 200);
     return () => clearInterval(interval);
   }, [value]);
 
-    useEffect(()=>{
-      if(value===100){
-          navigate('/auth/signup')
-      }
-    },[navigate, value])
+  useEffect(()=>{
+    if(value===100){
+      navigate('/auth/signin')
+    }
+    if(value===100 && user && token) {
+      navigate('/home');
+    }
+  },[navigate, token, user, value])
 
   return (
     <div className="w-full h-screen flex items-center justify-center relative">
