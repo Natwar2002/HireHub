b281763b5fa34f1276b96a50f3d954c3661b9a52
Natwar Patidar <natvarp6062@gmail.com>
2025-05-13 13:00:12 +0530

    Add UserDetails Projects and JobPost Schema

--- server/src/schema/jobPost.js
+++ server/src/schema/jobPost.js
@@ -4,19 +4,72 @@ const jobPostSchema = new mongoose.Schema({
     company: {
         type: String,
         required: true,
+        trim: true,
     },
     jobTitle: {
         type: String,
         required: true,
+        trim: true,
+        index: true,
     },
     jobDescription: {
+        type: [String],
+        required: true,
+    },
+    tags: {
+        type: [String],
+        required: true,
+        index: true,
+    },
+    requiredSkills: {
+        type: [String],
+        required: true,
+        index: true,
+    },
+    location: {
+        type: String,
+        required: true,
+        index: true,
+    },
+    experience: {
         type: String,
         required: true,
     },
-    requiredSkills: {
-
-    }
-});
+    responsibilities: {
+        type: [String],
+        required: true,
+    },
+    salary: {
+        min: {
+            type: Number,
+            required: true,
+        },
+        max: {
+            type: Number,
+            required: true,
+        },
+    },
+    postedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    jobType: {
+        type: String,
+        enum: ['Full-time', 'Part-time', 'Internship', 'Contract', 'Freelance'],
+        required: true,
+    },
+    deadLine: {
+        type: Date,
+        required: true,
+    },
+    applications: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+        }
+    ]
+}, { timestamps: true });
 
 const JobPost = mongoose.model('JobPost', jobPostSchema);
 
---
+++ server/src/schema/project.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+const projectSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    },
+    projectName: {
+        type: String,
+        required: true
+    },
+    projectDescription: {
+        type: [String],
+        required: true
+    },
+    liveLink: {
+        type: String,
+    },
+    githubLink: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true });
+
+const Projects = mongoose.model('Projects', projectSchema);
+
+export default Projects;
