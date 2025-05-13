1d04413a7da9c129a67bc8cb61fe0784434d54b3
aditya bhojane <bhojaneaditya7@gmail.com>
2025-05-13 23:44:51 +0530

    Add update

--- server/src/routes/v1/adminRouter.js
+++ server/src/routes/v1/adminRouter.js
@@ -1,10 +1,10 @@
 import { Router } from 'express'
-import { adminAuthApproveController, adminInviteController, adminSignInController } from '../../controllers/adminAuthController.js';
+import { adminSignInController } from '../../controllers/adminAuthController.js';
 
 const adminRouter = Router();
 
-adminRouter.post('/invite', adminInviteController);
-adminRouter.post('/request', adminAuthApproveController);
+// adminRouter.post('/invite', adminInviteController);
+// adminRouter.post('/request', adminAuthApproveController);
 adminRouter.post('/signin', adminSignInController);
 
 export default adminRouter;
