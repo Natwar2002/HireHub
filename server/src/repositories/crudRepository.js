a11335c8ed43caad44f263a521269987a72138d5
Natwar Patidar <natvarp6062@gmail.com>
2025-05-14 14:47:11 +0530

    Add apis to read, update and delete userDetails

--- server/src/repositories/crudRepository.js
+++ server/src/repositories/crudRepository.js
@@ -4,7 +4,7 @@ export default function crudRepository(modal) {
         create: async (data) => await modal.create(data),
         getById: async (id) => await modal.findById(id),
         getByEmail: async (email) => await modal.findById(email),
-        update: async (id, data) => await modal.findByIdAndUpdate(id, data),
+        update: async (id, data) => await modal.findByIdAndUpdate(id, { $set: data }, { new: true }),
         delete: async (id) => await modal.findByIdAndDelete(id),
         getAll: async () => await modal.find()
     }
--- server/src/routes/v1/userRouter.js
+++ server/src/routes/v1/userRouter.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { createUserDetailsController } from "../../controllers/userDetailsController.js";
+import { createUserDetailsController, deleteUserDetailsController, getUserDetailsController, updateUserDetailsController } from "../../controllers/userDetailsController.js";
 import { isAuthenticated } from '../../middlewares/authMiddleware.js';
 
 const userRouter = Router();
 
 userRouter.post('/userdetails', isAuthenticated, createUserDetailsController);
+userRouter.get('/userdetails', isAuthenticated, getUserDetailsController);
+userRouter.put('/userDetails', isAuthenticated, updateUserDetailsController);
+userRouter.delete('/userDetails', isAuthenticated, deleteUserDetailsController);
 
 export default userRouter;
