9c04bf52a637c97d0ea6abcca3f188f75f0afede
Natwar Patidar <natvarp6062@gmail.com>
2025-06-27 02:07:09 +0530

    Add code

---
+++ client/src/component/SiteLoader/SiteLoader.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import {  Progress } from "@heroui/react";
+import { useNavigate } from "react-router-dom";
+
+export const SiteLoader = () => {
+  const [value, setValue] = useState(0);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if(value >= 100) {
+        clearInterval(interval)
+        return;
+      }
+      setValue((v) => (v >= 100 ? 0 : v + 10));
+    }, 200);
+    return () => clearInterval(interval);
+  }, [value]);
+
+  useEffect(()=>{
+    if(value===100) {
+      navigate('/home');
+    }
+  },[navigate, value])
+
+  return (
+    <div className="w-full h-screen flex items-center justify-center relative">
+      <p className="absolute -translate-y-5">{value}</p>
+      <Progress isIndeterminate aria-label="Loading..." className="max-w-full" size="sm" />
+    </div>
+  );
+}
